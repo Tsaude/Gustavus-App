ee5046656e98bb4d265f67c1e79589700f6e197c
Tucker Saude <tuckersaude@Tuckers-MacBook-Pro.local>
2015-09-21 19:15:25 -0500

    udpated for iOS 9 and newer iPhones

--- GustavusApp/Controller/GABuildingViewController.m
+++ GustavusApp/Controller/GABuildingViewController.m
@@ -16,7 +16,7 @@
 
 @end
 
-@implementation GABuildingViewController{
+@implementation GABuildingViewController {
     UITextView * _textView;
     UINavigationBar * _navBar;
     UITableView * _tableView;
@@ -25,16 +25,8 @@
 
 }
 
--(void)viewWillAppear:(BOOL)animated{
-    
-}
-
-
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-    //[self buildNavBar];
     [self setUpNavBar];
     [self buildImageView];
     [self buildTextView];
@@ -43,7 +35,7 @@
 
 }
 
--(void)setUpNavBar{
+- (void)setUpNavBar {
     UIBarButtonItem *leftButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop
                                                                                 target:self
                                                                                 action:@selector(dismissBuildingView)];
@@ -51,77 +43,53 @@
     [self.navigationItem setTitle:self.building.title];
     
 }
--(void)buildScrollView{
-    _scrollView = [[A3ParallaxScrollView alloc] initWithFrame:CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-20)];
-    [_scrollView addSubview:_imageView withAcceleration:(CGPoint){0.5f, 0.5f}];
-    [_scrollView addSubview:_textView];
-    [_scrollView addSubview:_tableView];
-    [_scrollView setContentSize:CGSizeMake(self.view.frame.size.width, _textView.frame.size.height +_imageView.frame.size.height*.75f+_tableView.frame.size.height)];
-    [self.view addSubview:_scrollView];
-}
--(void)buildNavBar{
-    _navBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 64)];
-    [_navBar setBarTintColor:[UIColor darkTextColor]];
-    _navBar.translucent = YES;
-    UIBarButtonItem * back = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:self action:@selector(dismissBuildingView)];
-    UINavigationItem * titleItem = [[UINavigationItem alloc] initWithTitle:self.building.title];
-    [titleItem setLeftBarButtonItem:back];
-    _navBar.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
-    _navBar.items = @[titleItem];
-    [self.view addSubview:_navBar];
-}
--(void)buildImageView{
+
+- (void)buildImageView {
     //getting image
     UIImage * image;
     
-    NSLog(@"%@", self.building.pic);
     image = [UIImage imageNamed:[NSString stringWithFormat:@"%@.png", self.building.name]];
-    NSLog(@"%@", image);
     if(!image){
-        
-        
         image = [UIImage imageNamed:@"Chapel-Winter.png"];
-        NSLog(@"%@", image);
-        
-        
     }
     
     _imageView = [[UIImageView alloc]initWithImage:image];
+    CGRect frame = _imageView.frame;
+    if (frame.size.width < [UIScreen mainScreen].bounds.size.width) {
+        CGFloat diff = [UIScreen mainScreen].bounds.size.width - frame.size.width;
+        frame.size.width += diff;
+        frame.size.height += diff;
+    }
+    _imageView.frame = frame;
+    _imageView.contentMode = UIViewContentModeScaleAspectFill;
 }
--(void)buildTextView{
+
+- (void)buildTextView {
     NSURL * URLpath = [[NSBundle mainBundle]URLForResource:self.building.name withExtension:@"rtf"];
     
     if (!URLpath) {
         URLpath = [[NSBundle mainBundle]URLForResource:@"sample" withExtension:@"rtf"];
     }
     
-    NSMutableAttributedString*  mutableContent = [[NSMutableAttributedString alloc]initWithFileURL:URLpath
-                                                                                           options:@{NSDocumentTypeDocumentAttribute: NSRTFTextDocumentType}
+    NSMutableAttributedString*  mutableContent = [[NSMutableAttributedString alloc] initWithFileURL:URLpath
+                                                                                            options:@{NSDocumentTypeDocumentAttribute: NSRTFTextDocumentType}
                                                                                 documentAttributes:nil
                                                                                              error:nil];
-    
-    
-    
     for (int i = 0; i<[[mutableContent string] length]; i++) {
         NSRange  range = NSMakeRange(i, 1);
-        //NSLog(@"%hhd",[[[content attributesAtIndex:i effectiveRange:&range] objectForKey:@"font-weight"] isEqualToString:@"bold"]);
         NSDictionary * attrib = [mutableContent attributesAtIndex:i effectiveRange:&range];
         UIFont * font = [attrib objectForKey:@"NSFont"];
         BOOL isBold = [[font fontName] hasSuffix:@"-Bold"];
         if (isBold) {
             [mutableContent setAttributes:@{NSFontAttributeName: [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline]} range:NSMakeRange(i, 1)];
         }
-        else{
+        else {
             [mutableContent setAttributes:@{NSFontAttributeName: [UIFont preferredFontForTextStyle:UIFontTextStyleBody]} range:NSMakeRange(i, 1)];
         }
         
     }
     NSAttributedString * content = [[NSAttributedString alloc]initWithAttributedString:mutableContent];
-    
     CGSize textSize = [content size];
-    //CGFloat width = [UIScreen mainScreen].bounds.size.width;
-    //CGRect rect = [content boundingRectWithSize:CGSizeMake(width, 10000) options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading context:nil];
-    //CGSize textSize = rect.size;
     CGRect frameRect = CGRectMake(0, (_imageView.frame.size.height*.75), [[UIScreen mainScreen] bounds].size.width, textSize.height);
     _textView= [[UITextView alloc] initWithFrame:frameRect];
     [_textView setAttributedText:content];
@@ -130,17 +98,15 @@
     [_textView setUserInteractionEnabled:NO];
     [_textView sizeToFit];
     [_textView setFrame:CGRectMake(_textView.frame.origin.x, _textView.frame.origin.y, [UIScreen mainScreen].bounds.size.width, _textView.frame.size.height)];
-    //
-    //[_textView set]
 }
--(void)buildTableView{
+
+
+- (void)buildTableView {
     CGFloat tableViewHeight = 50;
     CGFloat tableViewy = (_imageView.frame.size.height*.75)+ _textView.frame.size.height;
     CGRect tableViewFrame = CGRectMake(0, tableViewy, [UIScreen mainScreen].bounds.size.width, tableViewHeight);
     
-    
     _tableView = [[UITableView alloc] initWithFrame:tableViewFrame style:UITableViewStylePlain];
-    
     _tableView.scrollEnabled = NO;
     _tableView.showsHorizontalScrollIndicator = NO;
     _tableView.showsVerticalScrollIndicator = NO;
@@ -151,19 +117,29 @@
     [_tableView setSeparatorStyle:UITableViewCellSeparatorStyleSingleLine];
 }
 
+
+- (void)buildScrollView {
+    _scrollView = [[A3ParallaxScrollView alloc] initWithFrame:CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-20)];
+    [_scrollView addSubview:_imageView withAcceleration:(CGPoint){0.5f, 0.5f}];
+    [_scrollView addSubview:_textView];
+    [_scrollView addSubview:_tableView];
+    [_scrollView setContentSize:CGSizeMake(self.view.frame.size.width, _textView.frame.size.height +_imageView.frame.size.height*.75f+_tableView.frame.size.height)];
+    [self.view addSubview:_scrollView];
+}
+
 - (void)dismissBuildingView {
     [self.navigationController.presentingViewController dismissViewControllerAnimated:YES completion:NULL];
 }
 
 
--(UIStatusBarStyle)preferredStatusBarStyle{
+- (UIStatusBarStyle)preferredStatusBarStyle {
     return UIStatusBarStyleLightContent;
 }
 
 
 
 //Table View Methods
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *CellIdentifier = @"cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
     
@@ -174,55 +150,37 @@
     if (indexPath.row == 0) {
         cell.textLabel.text = @"Sources";
     }
-    /*
-    if (indexPath.row == 1) {
-        cell.textLabel.text = @"Sources";
-    }
-     */
     
     return cell;
 }
 
--(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return 1;
 }
 
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.row == 0) {
-        /*NSLog(@"row 0 has been tapped!");
-        
-        
-        
-         */
         UITableViewCell * cell = [_tableView cellForRowAtIndexPath:indexPath];
         [cell setHighlighted:NO];
         [_tableView reloadData];
         [self performSegueWithIdentifier:@"sources" sender:self];
-        
     }
     
     if (indexPath.row == 1) {
-        NSLog(@"row 1 has been tapped!");
         UITableViewCell * cell = [_tableView cellForRowAtIndexPath:indexPath];
         [cell setHighlighted:NO];
         [_tableView reloadData];
-        
     }
 }
 
--(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
-    GABuildingViewController * buildingViewController = sender;
-    NSLog(@"%@", buildingViewController.building.title);
-    
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     GASourcesViewController * sources = segue.destinationViewController;
     [sources.navigationItem setTitle:@"Sources"];
     [sources setBuilding:self.building];
-    
-
 }
 
 @end
--- GustavusApp/Controller/GAMasterViewController.m
+++ GustavusApp/Controller/GAMasterViewController.m
@@ -18,34 +18,21 @@
 }
 @end
 
-@implementation GAMasterViewController{
+@implementation GAMasterViewController {
     UINavigationBar * _navBar;
 }
 
 
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    
-    //nav bar
-//    _navBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 64)];
-//    [_navBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
-//     _navBar.shadowImage = [UIImage new];
-//    [_navBar setBarTintColor:[UIColor darkTextColor]];
-//    _navBar.translucent = YES;
-//    UINavigationItem * titleItem = [[UINavigationItem alloc] initWithTitle:@"map"];
-//    _navBar.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
-//    _navBar.items = @[titleItem];
     
     //setting up map
     CGRect screenRect = self.view.bounds;
     self.gustavusMap = [[UIImageView alloc]initWithImage:[UIImage imageWithPDFNamed:@"gustavus.pdf"
                                                                            atHeight:800.0]];
-    [self.gustavusMap setUserInteractionEnabled:YES];
+    self.gustavusMap.userInteractionEnabled = YES;
     self.mapScrollView = [[UIScrollView alloc] initWithFrame:screenRect];
-    //[scrollView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
     [self.mapScrollView addSubview:self.gustavusMap];
     [self.mapScrollView setContentSize:self.gustavusMap.bounds.size];
     self.mapScrollView.minimumZoomScale = .5;
@@ -60,37 +47,11 @@
     
 }
 
-
-//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-//{
-//    if ([[segue identifier] isEqualToString:@"building"]) {
-//        UIButton * button = (UIButton * )sender;
-//        
-//        NSLog(@"button clicked %ld", (long)button.tag);
-//        Building * building = [[Building alloc]initWithName:[GAMasterViewController caseArrayforButtons][button.tag]];
-//        NSLog(@"%@", building.title);
-//        
-//        [segue.destinationViewController setBuilding:building];
-//    }
-//}
-
-
-
--(UIInterfaceOrientation)preferredInterfaceOrientationForPresentation{
-    return UIInterfaceOrientationPortrait;
-}
-
-- (NSUInteger)supportedInterfaceOrientations{
-    return UIInterfaceOrientationMaskPortrait;
-}
-
-- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
-{
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
     return self.gustavusMap;
 }
 
--(void)setUpButtons{
-    NSLog(@"settingUpButtons");
+-(void)setUpButtons {
     if ([[GABuildingManager sharedManager] buildingInfo]) {
         for (NSString * key in [[GABuildingManager sharedManager] buildingInfo]) {
             NSArray* framePointArray = [[[GABuildingManager sharedManager] buildingInfo][key] objectForKey:@"frameRect"];
@@ -111,52 +72,46 @@
             
         }
     }
-    else{
+    else {
         NSLog(@"Button Info Array was not initialized");
     }
-    
-    
 }
 
--(IBAction)buttonLetGo:(id)sender{
-    UIButton * button = (UIButton *)sender;
+-(IBAction)buttonLetGo:(id)sender {
+    UIButton *button = (UIButton *)sender;
     [button setBackgroundColor:[UIColor clearColor]];
 }
 
--(IBAction)buttonHeld:(id)sender{
-    
-    UIButton * button = (UIButton *)sender;
+-(IBAction)buttonHeld:(id)sender {
+    UIButton *button = (UIButton *)sender;
     [button setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:.3]];
-    
 }
 
--(IBAction)buttonClicked:(id)sender{
-    UIButton * button = (UIButton * )sender;
+-(IBAction)buttonClicked:(id)sender {
+    UIButton *button = (UIButton *)sender;
     [button setBackgroundColor:[UIColor clearColor]];
-    NSLog(@"button clicked %ld", (long)button.tag);
-    Building * building = [[Building alloc]initWithName:[GAMasterViewController caseArrayforButtons][button.tag]];
-    NSLog(@"%@", building.title);
-    UIStoryboard * storyboard = self.storyboard;
-    UINavigationController * navigationController =(UINavigationController *)[storyboard instantiateViewControllerWithIdentifier:@"buildingViewController"];
-    GABuildingViewController * buildingViewController;
-    if([[navigationController topViewController] isKindOfClass:[GABuildingViewController class]]){
+    
+    Building *building = [[Building alloc]initWithName:[GAMasterViewController caseArrayforButtons][button.tag]];
+    UIStoryboard *storyboard = self.storyboard;
+    UINavigationController *navigationController =(UINavigationController *)[storyboard instantiateViewControllerWithIdentifier:@"buildingViewController"];
+    
+    GABuildingViewController *buildingViewController;
+    
+    if([[navigationController topViewController] isKindOfClass:[GABuildingViewController class]]) {
         buildingViewController = (GABuildingViewController *)[navigationController topViewController];
         [buildingViewController setBuilding:building];
-        
     }
-    else
-    {
+    else {
         NSLog(@"top controller is not GABuildingViewController");
     }
 
     [self presentViewController:navigationController animated:YES completion:nil];
 }
 
-
-+(NSArray *)caseArrayforButtons{
++(NSArray *)caseArrayforButtons {
     static NSArray* caseArray = nil;
     
-    if(caseArray == nil){
+    if(caseArray == nil) {
         caseArray = @[@"arb",
                       @"arborview",
                       @"cabin",
